a5ae16872a25e7cce51b4d139ebaf08bee98823d
machty <machty@gmail.com>
2022-08-22 10:03:02 -0400

    signature and registry

--- app/components/foo.ts
+++ app/components/foo.ts
@@ -1,11 +1,22 @@
 import Component from '@glimmer/component';
 
-interface FooArgs {
-  bar: string;
+export interface FooSignature {
+  Args: {
+    bar: string;
+  };
+  Blocks: {
+    default: [string];
+  };
 }
 
-export default class Foo extends Component<FooArgs> {
+export default class Foo extends Component<FooSignature> {
   get barCapitalized() {
     return this.args.bar.toUpperCase();
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    Foo: typeof Foo;
   }
 }
